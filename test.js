79e9baf81bd07439da0343969e2572ce1cf2c6c0
Ciganyslayer <kovacsdavid480@gmail.com>
2026-01-08 12:05:44 +0100

    aaa

---
+++ test.js
@@ -0,0 +1,11 @@
+
+console.log("Hello, World!");
+
+function Kep_betesz() {
+    document.getElementById("kep_helye").innerHTML =
+    "<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKrtH3saDhvhN-4jMNzvWJVqSvCvnKd1QjkA&s' alt='kep'>"
+}
+
+function Kep_elrejt() {
+    document.getElementById("kep_helye").innerHTML = "";
+}
